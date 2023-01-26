89a974a4e0023e8c0e06ff2117af059a8756d37f
Leo <leoslegos1@gmail.com>
2023-01-26 15:23:20 -0800

    Prototyping cv with Phil's files as reference

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/eocvTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/eocvTests.java
@@ -2,62 +2,54 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.auto.JunctionPipeline;
+import org.firstinspires.ftc.teamcode.common.Constants;
+import org.firstinspires.ftc.teamcode.common.HardwareDrive;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
-@Autonomous(name="Auto: SkyStone Detector")
-public class eocvTests extends LinearOpMode {
-    // Handle hardware stuff...
+import java.util.ArrayList;
 
-    int width = 320;
-    int height = 240;
-    // store as variable here so we can access the location
-    JunctionPipeline detector = new JunctionPipeline(width);
+@Autonomous(name = "F2/A5 Safer auto", group = "Robot")
+public class eocvTests extends LinearOpMode {
     OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    // put detections, contours, generally variables to be filled here?
+
+    Constants constants = new Constants();
+    HardwareDrive robot = new HardwareDrive();
+    private CRServo serv0;
+    private final ElapsedTime runtime = new ElapsedTime();
+
 
     @Override
     public void runOpMode() {
-        // robot logic...
+        robot.init(hardwareMap);
+        serv0 = hardwareMap.get(CRServo.class, "serv0");
 
-        // https://github.com/OpenFTC/EasyOpenCV/blob/master/examples/src/main/java/org/openftc/easyopencv/examples/InternalCameraExample.java
-        // Initialize the back-facing camera
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "log920"), cameraMonitorViewId);
+        // JunctionPipeline = new JunctionPipeline;
 
-        camera.setPipeline(detector);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        // camera.setPipeline(JunctionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened() {
+                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
             public void onError(int errorCode) {
-
             }
         });
-        // Use the SkystoneDetector pipeline
-        // processFrame() will be called to process the frame
-        camera.setPipeline(detector);
-        // Remember to change the camera rotation
-        camera.startStreaming(width, height, OpenCvCameraRotation.SIDEWAYS_LEFT);
 
-        //...
-
-        JunctionPipeline.SkystoneLocation location = detector.getLocation();
-        if (location != JunctionPipeline.SkystoneLocation.NONE) {
-            telemetry.addLine("no skystone seen");
-        } else {
-            telemetry.addLine("skystone detected");
-        }
-
-        // more robot logic...
+        telemetry.setMsTransmissionInterval(50);
     }
 }
