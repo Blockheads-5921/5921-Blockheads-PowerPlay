f18d8b5d01df620a97a2286fe9542e6fca129854
Tine <apricitinecas@gmail.com>
2023-08-21 22:35:10 -0700

    UPDATE THE DOCS
    Signed-off-by: Tine <apricitinecas@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BaseDriveComplete.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/BaseDriveComplete.java
@@ -1,5 +1,9 @@
+/* go read HardwareDrive in the common folder first!! */
+
+/* tell android studio that we're in this directory */
 package org.firstinspires.ftc.teamcode.teleop;
 
+/* import our packages */
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -17,59 +21,99 @@ import org.firstinspires.ftc.teamcode.common.Constants;
 import org.firstinspires.ftc.teamcode.common.HardwareDrive;
 import org.firstinspires.ftc.teamcode.common.Utility;
 
+/* 
+woah :0 - this fancy thing is called a decorator and its only 
+purpose is to tell the driver station that you can run this program
+in the teleop menu; it also tells it its name
+*/
 @TeleOp(name = "Base Drive Complete", group = "Drive")
+/* 
+here, we're extending a class called LinearOpMode - LinearOpMode is 
+provided internally by the ftc sdk and is what you use when you want the 
+robot to move
+*/
 public class BaseDriveComplete extends LinearOpMode {
 
+    /* 
+    remember HardwareDrive? we're instatiating that class here; 
+    everything that we use to control our robot will use this variable 
+    */
     private final HardwareDrive robot = new HardwareDrive();
     private final ElapsedTime runtime = new ElapsedTime();
 
+    /* just the robot's speed */
     double drivePower = 0.40;
 
+    /* another decorater but idk what it does :/ */
     @Override
+    /*
+    this method is SUPER important!! it comes as a part of LinearOpMode
+    so every class that extends LinearOpMode must have this
+    method - basically, everything in this function will run when you press
+    the "init" button on the driver station with this opmode selected
+    */
     public void runOpMode() {
-        composeTelemetry();
-        robot.init(hardwareMap);
-        telemetry.addData("Say", "Hello Driver");
-        runtime.reset();
-        robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        composeTelemetry(); // prepare the robot's telemetry (just data that we want to get from it)
+        robot.init(hardwareMap); // remember?? call the init method on robot and pass the abstract hardwareMap variable (abstract meaning it inherently comes with the class LinearOpMode)
+        telemetry.addData("Say", "Hello Driver"); // just saying hi by calling addData on the abstract telemetry member
+        runtime.reset(); // reset the little timer on the driver station
+        robot.lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // super important!! reset our lift encoder so the lift doesn't break itself
+        robot.lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // now just tell the lift to use its encoder
+        waitForStart(); // basically nothing after this will run until start is pressed on the driver's station
 
-        waitForStart();
-
+        /* tell the imu to remember itself at the current position */
         robot.imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+        /* 
+        very important!! opModeIsActive is an abstract method that returns true if the opmode
+        is currently running - so we're just saying run all of this code in an infinite loop
+        until we stop running the opmode
+        */
         while (opModeIsActive()) {
-            UpdateGripper();
-            UpdateTelemetry();
-            if (gamepad1.right_bumper) drivePower = 1;
-            DriveTrainBase(drivePower);
-            DriveMicroAdjust();
+            UpdateGripper(); // update our gripper
+            UpdateTelemetry(); // update the telemetry
+            if (gamepad1.right_bumper) drivePower = 1; // if the right bumper on the first controller is pressed, increase the speed
+            DriveTrainBase(drivePower); // see the below method - we're just telling the robot to move at a certain speed
+            DriveMicroAdjust(); // just if we need to adjust the robot a tiny bit
         }
     }
 
     @Utility.Encapsulate
+    /* 
+    super important method!! this tells the robot how to react to different controller inputs -
+    it's pretty simple and just accepts a number on how fast the robot should go
+    */
     private void DriveTrainBase(double drivePower) {
-        double directionX = Math.pow(gamepad1.left_stick_x, 1);
-        double directionY = Math.pow(gamepad1.left_stick_y, 1);
-        double directionR = -Math.pow(gamepad1.right_stick_x, 1);
-        double liftPower = Math.pow(gamepad2.right_stick_y, 1);
+        double directionX = Math.pow(gamepad1.left_stick_x, 1); // the amount of horizontal direction that the left joystick on the first controller is pointing (this is strafing the robot)
+        double directionY = Math.pow(gamepad1.left_stick_y, 1); // the amount of vertical direction that the left joystick on the first controller is pointing (this is driving the robot)
+        double directionR = -Math.pow(gamepad1.right_stick_x, 1); // the amount of horizontal direction that the right joystick on the first controller is pointing (this is rotating the robot)
+        double liftPower = Math.pow(gamepad2.right_stick_y, 1); // the amount of vertical direction that the right joystick on the first controller is pointing (this is moving the lift)
 
+        /* make it so that controller stick drift isn't a problem by eliminating small amounts of input */
         if (gamepad1.left_stick_x < 0.2 && gamepad1.left_stick_x > -0.2) directionX = 0;
         if (gamepad1.left_stick_y < 0.2 && gamepad1.left_stick_y > -0.2) directionY = 0;
 
+        /* 
+        some math involved here - all that we're doing is multiplying the overall
+        direction of the input by the power setting each of the robot's motors to that 
+        */
         robot.lf.setPower((directionY + directionR - directionX) * drivePower);
         robot.rf.setPower((-directionY + directionR - directionX) * drivePower);
         robot.lb.setPower((directionY + directionR + directionX) * drivePower);
         robot.rb.setPower((-directionY + directionR + directionX) * drivePower);
 
-        int liftPos = robot.lift.getCurrentPosition();
-        if (liftPos < Constants.elevatorPositionTop && gamepad2.right_stick_y < 0) {
-            robot.lift.setPower((gamepad2.left_stick_y) * 0.1 - -0.001);
-        }
-        else if (liftPos > Constants.elevatorPositionBottom && gamepad2.right_stick_y > 0) {
-            robot.lift.setPower((gamepad2.left_stick_y) * 0.01);
-        } else robot.lift.setPower((liftPower - 0.001) * 1);
+        int liftPos = robot.lift.getCurrentPosition(); // get the lift's current position based on the encoder
+
+        /* 
+        some fancy checking to make sure that the lift doesn't explode -
+        if the elevator is already fully extended, make it so that it rises or lowers
+        at an extremely slow rate
+        */
+        if (liftPos < Constants.elevatorPositionTop && gamepad2.right_stick_y < 0) robot.lift.setPower((gamepad2.left_stick_y) * 0.1 - -0.001);
+        else if (liftPos > Constants.elevatorPositionBottom && gamepad2.right_stick_y > 0) robot.lift.setPower((gamepad2.left_stick_y) * 0.01);
+        else robot.lift.setPower((liftPower - 0.001) * 1);
     }
 
+    /* kind of not very useful, this is pretty self explanatory */
     private void DriveMicroAdjust() {
         if (gamepad1.dpad_up) {
             robot.lf.setPower(-0.4);
@@ -106,11 +150,13 @@ public class BaseDriveComplete extends LinearOpMode {
         }
     }
 
+    /* A simple method to update the gripper when the left or right triggers are pressed*/
     private void UpdateGripper() {
         if (gamepad2.left_trigger > 0.01) robot.serv0.setPower(0.22 * gamepad2.left_trigger - 0);
         else if (gamepad2.right_trigger > 0.01) robot.serv0.setPower(-0.16 * gamepad2.right_trigger + 0);
     }
 
+    /* simple stuff, we're just giving the telemetry stack info about the position of our lift and the joysticks */
     private void UpdateTelemetry() {
         telemetry.addData("g1.X", gamepad1.left_stick_x);
         telemetry.addData("g1.Y", -gamepad1.left_stick_y);
@@ -120,6 +166,11 @@ public class BaseDriveComplete extends LinearOpMode {
         telemetry.update();
     }
 
+    /* 
+    this is a little more advanced telemetry and is only run once
+    this only has to do with the imu though so if you wna to learn about that 
+    you can
+    */
     public void composeTelemetry() {
         telemetry.addAction(() -> {
             robot.angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
